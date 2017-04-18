0f74217885a73b9db8756fcafdb6bed73b15eba9
Aleks <Aleks@LOK111-05>
2017-04-18 11:46:54 +0200

    5 TO TEST OR NOT TO TEST - equals en hashCode

--- src/test/java/be/vdab/valueobjects/JaarTest.java
+++ src/test/java/be/vdab/valueobjects/JaarTest.java
@@ -1,8 +1,7 @@
 package be.vdab.valueobjects;
 
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;;
+import static org.junit.Assert.*;
 
 public class JaarTest {
 
@@ -24,5 +23,20 @@ public class JaarTest {
     @Test
     public void eenJaarNietDeelbaarDoor4IsGeenSchrikkeljaar() {
 	assertFalse(new Jaar(2015).isSchrikkeljaar());
+    }
+
+    @Test
+    public void equalsOpTweeDezelfdeJarenMoetTrueZijn() {
+	assertEquals(new Jaar(2015), new Jaar(2015));
+    }
+
+    @Test
+    public void equalsOpTweeVerschillendeJarenMoetFalseZijn() {
+	assertNotEquals(new Jaar(2015), new Jaar(2016));
+    }
+
+    @Test
+    public void hashCodeOpTweeDezelfdeJarenMoetGelijkZijn() {
+	assertEquals(new Jaar(2015).hashCode(), new Jaar(2015).hashCode());
     }
 }
