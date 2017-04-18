a617d18958bdfe1dc5a7efaa94219870ae197299
Aleks <Aleks@LOK111-05>
2017-04-18 09:38:04 +0200

    Kennismaking

--- src/main/java/be/vdab/valueobjects/Jaar.java
+++ src/main/java/be/vdab/valueobjects/Jaar.java
@@ -1,7 +1,7 @@
 package be.vdab.valueobjects;
 
 public class Jaar {
-    
+
     private final int jaar;
 
     public Jaar(int jaar) {
@@ -9,12 +9,6 @@ public class Jaar {
     }
 
     public boolean isSchrikkeljaar() {
-	if (jaar % 400 == 0) {
-	    return true;
-	}
-	if (jaar % 100 == 0) {
-	    return false;
-	}
-	return jaar % 4 == 0;
+	return jaar % 4 == 0 && jaar % 100 != 0 || jaar % 400 == 0;
     }
 }
